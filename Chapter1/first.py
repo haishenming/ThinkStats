71ed1f0482515d8b912d484a8de867ba948c939d
xiangyufan <xiangyufan@juwang.cn>
2018-01-18 12:56:06 +0800

    Initial commit

---
+++ Chapter1/first.py
@@ -0,0 +1,17 @@
+from Chapter1 import survey
+
+table = survey.Pregnancies()
+table.ReadRecords()
+print('Number of pregnancies', len(table.records))
+
+alive1 = 0
+alive_other = 0
+for r in table.records:
+    if r.nbrnaliv == 1:
+        if r.birthord == 1:
+            alive1 += 1
+        else:
+            alive_other += 1
+
+print(alive1)
+print(alive_other)
